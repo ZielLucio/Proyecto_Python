bef61246fb7e00638cdb262328dda082140e6811
Fedora <raziel_lucio@icloud.com>
2019-04-20 17:22:19 -0500

    add update client

--- main.py
+++ main.py
@@ -4,8 +4,11 @@ clients = 'pablo,ricardo,'
 def create_client(client_name):
     global clients
 
-    clients += client_name
-    _add_comma()
+    if client_name not in clients:
+        clients += client_name
+        _add_comma()
+    else:
+        print('Client already is in the client\'s list')
 
 
 def list_clients():
@@ -13,16 +16,47 @@ def list_clients():
     print(clients)
 
 
+def update_client(client_name, update_client_name):
+    global clients
+
+    if client_name in clients:
+        clients = clients.replace(client_name + ',', update_client_name + ', ')
+    else:
+        print('Client is not in clients list')
+
 def _add_comma():
     global clients
     clients +=','
 
 
+def _print_welcome():
+    print('WELCOME TO PLATZI VENTAS')
+    print('*' * 50)
+    print('What would you like to do today?')
+    print('[C]reate client')
+    print('[U]pdate client')
+    print('[D]elete client')
+
+def _get_client_name():
+    return input('What is the client name? ')
+
+
 if __name__ == '__main__':
-    list_clients()
+    _print_welcome()
 
-    create_client('David')
+    command = input()
+    command = command.upper()
 
-    list_clients
-
-    print(clients)
+    if command == 'C':
+        client_name = _get_client_name
+        create_client(client_name)
+        list_clients()
+    elif command == 'D':
+        pass
+    elif command == 'U':
+        client_name = _get_client_name()
+        update_client_name = input('What is the updated client name: ')
+        update_client(client_name, update_client_name)
+        list_clients()
+    else:
+        print('Invalid command')
