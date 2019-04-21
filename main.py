13bb7c6f3a36a1c0ed7b9bbe6d469f331af1c2a4
Fedora <raziel_lucio@icloud.com>
2019-04-20 21:28:05 -0500

    Agregando metodos CLUDS

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import sys
 clients = 'pablo,ricardo,'
 
 
@@ -24,6 +25,26 @@ def update_client(client_name, update_client_name):
     else:
         print('Client is not in clients list')
 
+
+def delete_client(client_name):
+    global clients
+
+    if client_name in clients:
+        clients = clients.replace(client_name + ',', '')
+        pass
+    else:
+        print('Clients is not in clients list')
+
+
+def search_client(client_name):
+    clients_list = clients.split(',')
+    for client in clients_list:
+        if client != client_name:
+            continue
+        else:
+            return True
+
+
 def _add_comma():
     global clients
     clients +=','
@@ -34,11 +55,26 @@ def _print_welcome():
     print('*' * 50)
     print('What would you like to do today?')
     print('[C]reate client')
+    print('[L]ist clients')
     print('[U]pdate client')
     print('[D]elete client')
+    print('[S]earch client')
+    """print('*' * 50)"""
+    print('')
 
 def _get_client_name():
-    return input('What is the client name? ')
+    client_name = None
+    while not client_name:
+        client_name = input('What is the client name? ')
+
+        if client_name == 'exit':
+            client_name = None
+            break
+
+    if not client_name:
+        sys.exit()
+
+    return client_name
 
 
 if __name__ == '__main__':
@@ -48,15 +84,26 @@ if __name__ == '__main__':
     command = command.upper()
 
     if command == 'C':
-        client_name = _get_client_name
+        client_name = _get_client_name()
         create_client(client_name)
         list_clients()
+    elif command == 'L':
+        list_clients()
     elif command == 'D':
-        pass
+        client_name = _get_client_name()
+        delete_client(client_name)
+        list_clients()
     elif command == 'U':
         client_name = _get_client_name()
         update_client_name = input('What is the updated client name: ')
         update_client(client_name, update_client_name)
         list_clients()
+    elif command == 'S':
+        client_name = _get_client_name()
+        found = search_client(client_name)
+        if found:
+            print('The client is in the client\'s list')
+        else:
+            print('The client: {} is not in our client\'s list'.format(client_name))
     else:
         print('Invalid command')
